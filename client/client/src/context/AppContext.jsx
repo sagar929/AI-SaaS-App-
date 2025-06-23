bc50847239f5b1416a337c2bdf306eba0284a5c9
sagar929 <sagarshrama130804@gmail.com>
2025-06-23 11:20:35 +0530

    routing and navbar are completed

---
+++ client/client/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props)=>{
+    const [user,setUser] =  useState(true);
+
+    const value ={
+        user,setUser
+    }
+    return (
+        <AppContext.Provider value={value}>
+           {props.children}
+        </AppContext.Provider>
+    )
+
+}
+
+export default AppContextProvider
---
+++ client/client/src/pages/BuyCredit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BuyCredit = () => {
+  return (
+    <div>
+      BuyCredit
+      
+    </div>
+  )
+}
+
+export default BuyCredit
